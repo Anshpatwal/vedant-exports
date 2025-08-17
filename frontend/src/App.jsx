ec10dd05d6467a5c2537b8e52e56a236fc1eef72
Anshpatwal <anshpatwal2703@gmail.com>
2025-08-17 14:39:02 +0530

    backend fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ export default function Home() {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/api/send-inquiry', {
+      const res = await fetch('https://vedant-exports-2rqe.onrender.com/api/send-inquiry', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(inquiry)
